222a6048a819fd4e971ec58cfa0372e57f4c605c
conor quarry <conordquarry@gmail.com>
2021-06-01 15:44:30 +0100

    tempupdate

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,15 +1,15 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [ :home, :search ]
 
   def home
   end
 
   def search  
     if params[:search].blank?  
-      redirect_to(#HOME => root_path #, alert: "Empty field!") and return  
+     # redirect_to()#HOME => root_path #, alert: "Empty field!") and return 
     else  
-      @parameter = params[:search].downcase  
-      @results = Users.where("lower(name) LIKE :search", search: @parameter)   
-  
+      @parameter = params[:search]  
+      @results = User.where("city ILIKE :search", search: @parameter)    
     end  
   end
+end
