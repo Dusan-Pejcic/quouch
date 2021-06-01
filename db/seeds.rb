97145050d11c5e706d40267641bfef2595345162
mara <marameddy@yahoo.fr>
2021-06-01 15:15:35 +0200

    seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Review.destroy_all
+
+puts "Seeding..."
+User.create!( name: 'Med',
+  address: 'berlin, an der strasse',
+  city: 'Berlin',
+  username:'mara',
+  bio: 'about me',
+  email:  'maramm@yahoo.fr',
+  password: 123456
+
+)
+
+User.create!( name: 'doraa',
+  address: 'köln, an der strasse',
+  city: 'köln',
+  username:'doraa',
+  bio: 'about me',
+  email:  'dora123@yahoo.fr',
+  password: 123456
+
+)
+User.create!( name: 'nora ok',
+  address: 'berlin, an der strasse',
+  city: 'Berlin',
+  username:'noranora',
+  bio: 'about me',
+  email:  'noranora@yahoo.fr',
+  password: 123456
+
+)
+
+puts 'User created.'
