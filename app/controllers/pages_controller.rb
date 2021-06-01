64e1caaf882df92e3601929e3f7c86f03d27fb75
conor quarry <conordquarry@gmail.com>
2021-06-01 13:43:08 +0100

    updatedsearchpagebackened

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,13 @@ class PagesController < ApplicationController
 
   def home
   end
-end
+
+  def search  
+    if params[:search].blank?  
+      redirect_to(root_path, alert: "Empty field!") and return  
+    else  
+      @parameter = params[:search].downcase  
+      @results = #ENTERTABLENAMEHERE.all.where("lower(name) LIKE :search", search: @parameter)   
+  
+    end  
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -3,3 +3,6 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
+  get '/search' => 'pages#search', :as => 'search_page'
+end
