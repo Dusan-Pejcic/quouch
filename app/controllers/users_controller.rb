b13deafd762afa46c7fc08403d68746a686d5cf0
mara <marameddy@yahoo.fr>
2021-06-02 16:36:54 +0200

    update from master

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,18 @@
 class UsersController < ApplicationController
-    skip_before_action :authenticate_user!, only: [ :home, :search, :show ]
+  skip_before_action :authenticate_user!, only: [ :home, :search, :show ]
 
-    def index
-        @users = User.all
-    end
+  def index
+    @users = User.all
+  end
 
-    def show
-        @user = User.find(params[:id])
-    end
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+    authorize @user
+  end
+
+
 end
-
