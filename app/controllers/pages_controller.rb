777123217717b86dbfb1b7aa574a8a875716a4cd
conor quarry <conordquarry@gmail.com>
2021-06-02 11:40:04 +0100

    finishedsearchfunction

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,18 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [ :home, :search ]
 
   def home
+  end
+
+  def search  
+    
+  end
+  def result
+    if params[:search].blank?  
+      redirect_to(root_path, alert: "Empty field!") and return   
+     else  
+       @parameter = params[:search]  
+       @results = User.where("city ILIKE :search", search: @parameter)    
+     end  
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get '/search' => 'pages#search', :as => 'search_page'
+
+  get '/result' => 'pages#result', :as => 'result_page'
 end
